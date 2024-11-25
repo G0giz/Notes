5b16cb4a6ded9d41f55565f429a4c2eba03ea1e1
G0giz <73425532+G0giz@users.noreply.github.com>
2024-11-25 20:32:35 +0200

    Add fade-in effect to added note + add time for the note object

--- main.js
+++ main.js
@@ -1,38 +1,30 @@
 "use strict";
-// Get the elements of the note.
+
+//Get the elements of the form.
 const noteTitleBox = document.getElementById("noteTitleBox");
 const noteDescriptionBox = document.getElementById("noteDescriptionBox");
 const formFileBox = document.getElementById("formFileBox");
 const noteDateBox = document.getElementById("noteDateBox");
 const cardsContainer = document.getElementById("cardsContainer");
 
+// Get the notes from local storage if there is no data create empty array.
+let notes = JSON.parse(localStorage.getItem('notes')) || [];;
 
-// try to get the notes from local storage if there is no items create a blank array
-let notes = JSON.parse(localStorage.getItem('notes')) || [];
-
-
-// Function to load notes from local storage and display them
-function loadNotesFromLocalStorage() {
-
-    // Retrieve the notes from local storage
-    const notesFromStorage = JSON.parse(localStorage.getItem('notes')) || [];
-
-    // Update the global notes array with the data from local storage
-    notes = notesFromStorage;
-
-    // Display notes in cards
-    displayDataInCards();
-}
+//On load get the notes and display.
+window.onload = displayDataInCards;
 
 
-//On load get the notes
-window.onload = loadNotesFromLocalStorage;
-
-
-function addNote(event) {
-    event.preventDefault();
-    addData();
-    displayDataInCards();
+function addNote() {
+    try {
+        addData();
+        //Display the notes after 2 seconds because the fade-in effect.
+        //I want the last note to display with fade-in and then to remove
+        //from all the cards divs this class.
+        setTimeout(displayDataInCards,2000);
+    }
+    catch (err) {
+        alert(err.message);
+    }
 }
 
 
@@ -43,85 +35,170 @@ function addData() {
     const noteTitle = noteTitleBox.value;
     const noteDescription = noteDescriptionBox.value;
     const noteDate = noteDateBox.value;
-    const formFile = formFileBox.files[0];  // Get the actual file, not just its name
+    //Get the actual file, not just its name.
+    const formFile = formFileBox.files[0];  
 
-    // Validation checks of title and description
-    if (noteTitle === "") {
-        alert("Title can't be empty.");
-        noteTitleBox.focus();
-        return;
-    }
-    if (noteDescription === "") {
-        alert("Description can't be empty.");
-        noteDescriptionBox.focus();
-        return;
-    }
-
-    // If a file is selected, generate an object URL for it
+    //If a file is selected, generate an object URL for it.
     const formFileURL = formFile ? URL.createObjectURL(formFile) : '';
 
-    // Create the note object
+    //Get the date and time string from the datetime-local value.
+
+    const date = getDateFromNoteDate(noteDate);
+    const time = getTimeFromNote(noteDate);
+
+    //Create the note object
     const note = {
         id,
         noteTitle,
         noteDescription,
-        noteDate,
-        fileName: formFile ? formFile.name : '',  // Store file name
-        formFileURL  // Store the generated URL
+        date,
+        time,
+        //Store file name.
+        fileName: formFile ? formFile.name : '',
+        //Store the generated URL.
+        formFileURL  
     };
-    
-    // Add the note to the notes array
-    notes.push(note); 
-    // Save the updated notes array to local storage
+
+    // Add the note to the notes array.
+    notes.push(note);
+    // Save the updated notes array to local storage.
     localStorage.setItem('notes', JSON.stringify(notes));
-    clearUI();
-    // Refresh the display with the new note
-    displayDataInCards(); 
+    clearFields();
+    // Refresh the display the new note
+    displayLastAddedNote(note);
 }
 
+// Function to get the date from the date string.
+function getDateFromNoteDate(noteDate) {
 
-//Function to display the notes in cards
-function displayDataInCards() {
+    let date;
+    // If date empty return an empty string.
+    if (noteDate === "") {
+        return "";
+    }
+    else {
+        for (let i = 0; i < noteDate.length; i++) {
+            if (noteDate[i] == "T") {
+                // Cut the string for return the date from index 0 until the index of 'T'.
+                date = noteDate.substring(0, i);
+                return date;
+            }
+        }
+    }
+
+}
+
+// Function to get the time from the date string.
+function getTimeFromNote(noteDate) {
+
+    let time;
+    // If date empty return an empty string.
+    if (noteDate === "") {
+        return "";
+    }
+    else {
+        for (let i = 0; i < noteDate.length; i++) {
+            if (noteDate[i] == "T") {
+                //Cut the string for return the time from 1 index after the 'T' until the end.
+                time = noteDate.substring(i + 1, noteDate.length);
+                return time;
+            }
+        }
+    }
+}
+
+//Add the last note and display with fade-in (fade-in class).
+function displayLastAddedNote(note){
+
     let content = "";
-    for (const note of notes) {
-        content += `
-        <div class="card" id="card-${note.id}">
-            <!-- Three dots menu button -->
-            <div class="menu-container">
-                <button class="menu-button" onclick="toggleMenu(${note.id})">⋮</button>
-                <div class="menu-options" id="menu-${note.id}" style="display: none;">
-                    <button class="edit" onclick="editCard(${note.id})">✏️ Edit</button>
-                    <button class="save" onclick="saveCard(${note.id})" style="display:none;">💾 Save</button>
-                    <button class="delete" onclick="deleteNote(${note.id})">❌ Delete</button>
-                </div>
-            </div>
-            
-            <!-- Note content on the right side -->
-            <div class="card-content">
-                <span class="noteTitle inputWidth">${note.noteTitle}</span>
-                <span class="noteDescription inputWidth">${note.noteDescription}</span>
-                    <div>
-                    <a class="noteFile inputWidth" href="${note.formFileURL}" target="_blank">${note.fileName}</a>
-                    </div>
-                <span class="noteDate inputWidth">${note.noteDate}</span>
+    content += `
+    <div class="card fade-in" id="card-${note.id}">
+        <!-- Three dots menu button -->
+        <div class="menu-container">
+            <button class="menu-button" onclick="toggleMenu(${note.id})">⋮</button>
+            <div class="menu-options" id="menu-${note.id}" style="display: none;">
+                <button class="edit" onclick="editCard(${note.id})">✏️ Edit</button>
+                <button class="save" onclick="saveCard(${note.id})" style="display:none;">💾 Save</button>
+                <button class="delete" onclick="deleteNote(${note.id})">❌ Delete</button>
             </div>
         </div>
-        `;
-    }
-    cardsContainer.innerHTML = content;
+       
+        <div class="cardContent">
+            <span id='noteEditTitle-${note.id}' class="noteTitle inputWidth">${note.noteTitle}</span>
+            <span id='noteEditDescription-${note.id}' class="noteDescription inputWidth">${note.noteDescription}</span>
+                <div>
+                <a id='noteEditFileName-${note.id}' class="noteFile inputWidth" href="${note.formFileURL}" target="_blank">${note.fileName}</a>
+                </div>
+            <span id='noteEditDate-${note.id}' class="noteDate inputWidth">${note.date}</span>
+            <span id='noteEditTime-${note.id}' class="noteTime inputWidth">${note.time}</span>
+        </div>
+    </div>
+    `;
+    cardsContainer.innerHTML += content;
+}
+
+//Function to display the notes in cards without fade-in
+function displayDataInCards() {
+
+    let content = "";
+     for (const note of notes) {
+         content += `
+         <div class="card" id="card-${note.id}">
+             <!-- Three dots menu button -->
+             <div class="menu-container">
+                 <button class="menu-button" onclick="toggleMenu(${note.id})">⋮</button>
+                 <div class="menu-options" id="menu-${note.id}" style="display: none;">
+                     <button class="edit" onclick="editCard(${note.id})">✏️ Edit</button>
+                     <button class="save" onclick="saveCard(${note.id})" style="display:none;">💾 Save</button>
+                     <button class="delete" onclick="deleteNote(${note.id})">❌ Delete</button>
+                 </div>
+             </div>
+            
+             <div class="cardContent">
+                 <span id='noteEditTitle-${note.id}' class="noteTitle inputWidth">${note.noteTitle}</span>
+                 <span id='noteEditDescription-${note.id}' class="noteDescription inputWidth">${note.noteDescription}</span>
+                     <div>
+                     <a id='noteEditFileName-${note.id}' class="noteFile inputWidth" href="${note.formFileURL}" target="_blank">${note.fileName}</a>
+                     </div>
+                 <span id='noteEditDate-${note.id}' class="noteDate inputWidth">${note.date}</span>
+                 <span id='noteEditTime-${note.id}' class="noteTime inputWidth">${note.time}</span>
+             </div>
+         </div>
+         `;
+     }
+
+    cardsContainer.innerHTML = content; 
+}
+
+// Function to remove the class that incharge to remove the class from the div note.
+function removeFadeInClassFromNotes(){
+
+    for (const note of notes) {
+
+        //Get the element of the card.
+        var element = document.getElementById(`card-${note.id}`);
+        //Check if the element contain the class fade-in
+        let isMainPresent = element.classList.contains("fade-in");
+        if(isMainPresent)
+        {
+            //If true remove the class.
+            element.classList.remove("fade-in");
+        }
+
+   }
 }
 
 
-// Function to toggle the specific menu visibility
+//Function to toggle the specific three dots menu visibility.
 function toggleMenu(id) {
 
-    // Handle the menu display
+    //Handle the three dots menu display.
     const menu = document.getElementById(`menu-${id}`);
     menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
 
-    // Close the menu if clicked outside
+    //Close the three dots menu if clicked outside.
     document.addEventListener('click', function handleOutsideClick(event) {
-        // Check if the click is outside the menu-container
+        //Check if the click is outside the menu-container.
         if (!event.target.closest(`#card-${id} .menu-container`)) {
             menu.style.display = 'none';
             document.removeEventListener('click', handleOutsideClick); // Remove the listener after it closes
@@ -130,15 +207,16 @@ function toggleMenu(id) {
 }
 
 
-// Delete one item: 
+//Function for Delete one item
 function deleteNote(id) {
 
-    const sure = confirm("Are you sure?");
+    let note = getNoteById(id);
+    const sure = confirm(`Are you sure you want to delete ${note.noteTitle} ?`);
 
-    // Check if  the user choose no
+    //Check if  the user choose no.
     if (!sure) return;
 
-    // Get the index of the item that need to be deleted
+    //Get the index of the item that need to be deleted.
     let index = 0;
     for (let i = 0; i < notes.length; i++) {
         if (notes[i].id === id) {
@@ -147,36 +225,49 @@ function deleteNote(id) {
         }
     }
 
-    // Delete from this index only one item.
-    notes.splice(index, 1); 
+    //Delete from this index only one item.
+    notes.splice(index, 1);
 
-    // Save the updated notes array to local storage
+    //Save the updated notes array to local storage.
     localStorage.setItem('notes', JSON.stringify(notes));
     displayDataInCards();
 }
 
+//Function to return the note by id.
+function getNoteById(id) {
+    for (let i = 0; i < notes.length; i++) {
+        if (notes[i].id === id) {
+            return notes[i];
+        }
+    }
+}
 
 function editCard(id) {
 
+    //Get the card element by id.
     const card = document.getElementById(`card-${id}`);
 
-    // Find the note by ID using find.
-    const note = notes.find(note => note.id === id);
+    let note = getNoteById(id);
 
-    // Get the current card content
-    const noteTitleElement = card.querySelector('.noteTitle');
-    const noteDescriptionElement = card.querySelector('.noteDescription');
-    const noteFileElement = card.querySelector('.noteFile');
-    const noteDateElement = card.querySelector('.noteDate');
-    const fileNameTemp = "";
+    //Get the current card content inputs by classes.
+    const noteTitleElement = document.getElementById(`noteEditTitle-${id}`);
+    const noteDescriptionElement = document.getElementById(`noteEditDescription-${id}`);
+    const noteFileElement = document.getElementById(`noteEditFileName-${id}`);
+    const noteDateElement = document.getElementById(`noteEditDate-${id}`);
+    const noteTimeElement = document.getElementById(`noteEditTime-${id}`);
 
-    // Change content to editable fields
+    //Hide the element when the user edit the data.
+    noteTimeElement.style.display = "none";
+
+    //Change content to editable fields.
+    //The values of the inputs are the data of the note.
     noteTitleElement.innerHTML = `<input type="text" id="editTitle-${id}" value="${note.noteTitle}">`;
     noteDescriptionElement.innerHTML = `<textarea id="editDescription-${id}">${note.noteDescription}</textarea>`;
-    noteFileElement.innerHTML = `<input type="file" id="editFile-${id}">${fileNameTemp}</input>`;
-    noteDateElement.innerHTML = `<input type="date" id="editDate-${id}" value="${note.noteDate}">`;
+    noteFileElement.innerHTML = `<input type="file" id="editFile-${id}">`;
+    //Assign the value of the date and time with T as the format should be.
+    noteDateElement.innerHTML = `<input type="datetime-local" id="editDate-${id}" value="${note.date}T${note.time}">`;
 
-    // Show the Save button and hide the Edit button
+    //Show the Save button and hide the Edit button.
     card.querySelector('.edit').style.display = 'none';
     card.querySelector('.save').style.display = 'inline-block';
 }
@@ -186,44 +277,60 @@ function saveCard(id) {
 
     const card = document.getElementById(`card-${id}`);
 
-    // Get the edited values from the inputs
+    //Get the edited values from the inputs
     const editedTitle = document.getElementById(`editTitle-${id}`).value;
     const editedDescription = document.getElementById(`editDescription-${id}`).value;
     const editFile = document.getElementById(`editFile-${id}`);
     const editedDate = document.getElementById(`editDate-${id}`).value;
 
-    // Update the note in the notes array
-    const note = notes.find(note => note.id === id);
+    //Get the new date and time value from the input.
+    const date = getDateFromNoteDate(editedDate);
+    const time = getTimeFromNote(editedDate);
+
+    let note = getNoteById(id);
+    
     note.noteTitle = editedTitle;
     note.noteDescription = editedDescription;
 
-    // Check if the user did not choose a file and keep the last name
+    //Check if a file was selected
     const formFile = editFile.files[0];
-    if (formFile === undefined) {
-        note.fileName = note.fileName;
-    }
-    else{
+
+    if (formFile) {
+        //Generate a temporary object URL for the selected file.
+        const formFileURL = URL.createObjectURL(formFile);
+        //Update the note with the new file details.
         note.fileName = formFile.name;
+        // Store the generated URL in the note.
+        note.formFileURL = formFileURL; 
+    } else {
+        //If no file is selected, retain the existing file name and URL.
+        note.fileName = note.fileName;
+        note.formFileURL = note.formFileURL;
     }
-    note.noteDate = editedDate;
+
+    note.date = date;
+    note.time = time;
+
+    //Update the local storage.
+    localStorage.setItem('notes', JSON.stringify(notes));
 
     displayDataInCards();
 
-    // Hide the Save button and show the Edit button again
+    //Hide the Save button and show the Edit button again.
     card.querySelector('.edit').style.display = 'inline-block';
     card.querySelector('.save').style.display = 'none';
 
 }
 
 
-// Reset boxes: 
-function clearUI() {
+//Reset Inputs: 
+function clearFields() {
 
-    // Clear boxes:
+    //Clear inputs.
     noteTitleBox.value = "";
     noteDescriptionBox.value = "";
     formFileBox.value = "";
     noteDateBox.value = "";
-    // Put focus in the item box:
+    //Put focus in the title input.
     noteTitleBox.focus();
 }
